05e57c7f8cfe898be5dbd81229ef7a34195dc768
ellepannitto <ellepannitto@gmail.com>
2022-05-19 11:42:28 +0200

    parse speakers

--- experiments/parse_10speakers.py
+++ experiments/parse_10speakers.py
@@ -12,8 +12,8 @@ model_path = sys.argv[3]
 
 with Pool(10) as p:
 
-    iter_input_dir = [input_dir_basename+"{}/".format(str(n).zfill(2) for n in range(1, 11))]
-    iter_output_dir = [output_dir_basename+"{}/".format(str(n).zfill(2) for n in range(1, 11))]
+    iter_input_dir = [input_dir_basename+"{}/".format(x) for x in [str(n).zfill(2) for n in range(1, 11)]]
+    iter_output_dir = [output_dir_basename+"{}/".format(x) for x in [str(n).zfill(2) for n in range(1, 11)]]
     iter_model = [model_path for _ in range(1, 11)]
 
 #    for el in zip(iter_output_dir, iter_input_dir, iter_model):
