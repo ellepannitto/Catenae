7ef39ee22ed54841efc71a4f0e862b9b12f17a64
ellepannitto <ellepannitto@gmail.com>
2022-05-25 17:27:15 +0200

    weight exp

---
+++ catenae/core/analysis.py
@@ -0,0 +1,6 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+def correlate(output_dir, first_list, second_list):
+    pass
---
+++ experiments/weight_10speakers.py
@@ -0,0 +1,19 @@
+import sys
+import catenae
+
+from multiprocessing import Pool
+
+output_dir_basename = sys.argv[1]
+input_dir_basename = sys.argv[2]
+
+
+with Pool(3) as p:
+    
+    iter_output_dir = [output_dir_basename+"{}/".format(x) for x in [str(n).zfill(2) for n in range(1, 11)]]
+
+    iter_items_fpaths = [input_dir_basename+"{}/items-freq-summed.gz".format(x) for x in [str(n).zfill(2) for n in range(1, 11)]]
+    iter_totals_fpaths = [input_dir_basename+"{}/totals-freq-summed.gz".format(x) for x in [str(n).zfill(2) for n in range(1, 11)]]
+    iter_catenae_fpaths = [input_dir_basename+"{}/catenae-freq-summed.gz".format(x) for x in [str(n).zfill(2) for n in range(1, 11)]]
+
+    p.starmap(catenae.core.extraction.weight_catenae, zip(iter_output_dir, 
+                                                          iter_items_fpaths, iter_totals_fpaths, iter_catenae_fpaths))
