6f693ed5eb5b957e6ed4276df46d3394d969034b
ellepannitto <ellepannitto@gmail.com>
2022-10-18 17:18:43 +0200

    changed os to path

--- catenae/utils/files_utils.py
+++ catenae/utils/files_utils.py
@@ -1,6 +1,13 @@
+"""_summary_
+
+Returns:
+    _type_: _description_
+"""
 import os
 import glob
 import logging
+
+from pathlib import Path
 
 from typing import List
 
@@ -9,24 +16,37 @@ logger = logging.getLogger(__name__)
 
 
 def get_filenames(input_dir: str) -> List[str]:
+    """_summary_
+
+    Args:
+        input_dir (str): _description_
+
+    Returns:
+        List[str]: _description_
+    """
 
     if os.path.isdir(input_dir):
         return glob.glob(input_dir+"/*", recursive=True)
-    else:
-        return [input_dir]
+
+    return [input_dir]
 
 
-def check_or_create_dir(path: str) -> None:
+def check_or_create_dir(path: str) -> Path:
     """_summary_
 
     Args:
         path (str): _description_
+
+    Returns:
+        Path: _description_
     """
+    
+    p = Path(path)
 
-    if not os.path.exists(path):
-        logger.info(f"Creating folder {path}")
-        os.mkdir(path)
+    if not p.exists():
+        logger.info("Creating folder %s", path)
+        p.mkdir(parents=True)
     else:
-        logger.info(f"Checked path: {path}")
+        logger.info("Checked path: %s", path)
 
-    return path
+    return p
