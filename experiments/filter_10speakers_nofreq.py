ef3de6521ce9de721dca3ecc8e9615c2c32a0b68
ellepannitto <ellepannitto@gmail.com>
2022-10-23 11:55:31 +0200

    new experiment no thresholds

---
+++ experiments/filter_10speakers_nofreq.py
@@ -0,0 +1,34 @@
+"""_summary_
+"""
+import sys
+
+from multiprocessing import Pool
+
+import catenae
+
+
+output_dir_basename = sys.argv[1]
+input_dir_basename = sys.argv[2]
+
+FREQUENCY_THRESHOLD = 0
+WEIGHT_THRESHOLD = 0
+MIN_LEN_CATENA = 0
+MAX_LEN_CATENA = 5
+
+
+with Pool(3) as p:
+
+    iter_output_dir = [f"{output_dir_basename}/{x}/"
+                       for x in [str(n).zfill(2) for n in range(1, 11)]]
+    iter_input_file = [f"{input_dir_basename}/{x}/catenae-weighted.gz"
+                       for x in [str(n).zfill(2) for n in range(1, 11)]]
+
+    iter_frequency_threshold = [FREQUENCY_THRESHOLD for _ in range(1, 11)]
+    iter_weight_threshold = [WEIGHT_THRESHOLD for _ in range(1, 11)]
+    iter_min_len_catena = [MIN_LEN_CATENA for _ in range(1, 11)]
+    iter_max_len_catena = [MAX_LEN_CATENA for _ in range(1, 11)]
+
+    p.starmap(catenae.core.extraction.filter_catenae,
+              zip(iter_output_dir, iter_input_file,
+                  iter_frequency_threshold, iter_weight_threshold,
+                  iter_min_len_catena, iter_max_len_catena))
