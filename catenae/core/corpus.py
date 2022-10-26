43a3fb80eae6fa217105e78542dd1f7d1836d0b1
ellepannitto <ellepannitto@gmail.com>
2022-10-26 11:48:21 +0200

    pylint

--- catenae/core/corpus.py
+++ catenae/core/corpus.py
@@ -1,12 +1,19 @@
+# pylint: disable=unspecified-encoding
+"""_summary_
+
+Returns:
+    _type_: _description_
+"""
 import os
 import sys
 import logging
 import glob
 import random
 
-import ufal.udpipe as udpipe
+import ufal.udpipe as udpipe # pylint: disable=consider-using-from-import
 
 from catenae.utils import corpus_utils as cutils
+
 
 logger = logging.getLogger(__name__)
 
@@ -21,7 +28,7 @@ def reservoir_tokens_number(files, size, seed=42):
     considered_tokens = 0
     sentence_number = -1
     for filename in files:
-        logger.info("reading file {}".format(filename))
+        logger.info("reading file %s", filename)
 
         for sentence in cutils.plain_conll_reader(filename):
 
@@ -37,13 +44,13 @@ def reservoir_tokens_number(files, size, seed=42):
                     res[j] = sentence_number
                     len_res[j] = len(sentence)
 
-    x = 0
-    i = 0
-    while x < size and i < len(res):
-        x += len_res[i]
-        i = i+1
+    cur_size = 0
+    cur_len = 0
+    while cur_size < size and cur_len < len(res):
+        cur_size += len_res[cur_len]
+        cur_len = cur_len+1
 
-    return list(sorted(res[:i]))
+    return list(sorted(res[:cur_len]))
 
 
 def sample(output_dir, input_dir, size, seed):
@@ -51,7 +58,7 @@ def sample(output_dir, input_dir, size, seed):
     input_files = glob.glob(input_dir+"/*")
     random.Random(seed).shuffle(input_files)
 
-    L = len(input_files)
+    L = len(input_files) # pylint:disable=invalid-name
 
     training_files, valid_files, test_files = input_files[:int(L*0.8)], \
                                               input_files[int(L*0.8):int(L*0.9)], \
@@ -60,8 +67,8 @@ def sample(output_dir, input_dir, size, seed):
     training_size = int(size)
     valid_size, test_size = int(size*0.1), int(size*0.1)
 
-    logger.info("Setting training size to {}, valid size to {} and test size to \
-                {} words".format(training_size, valid_size, test_size))
+    logger.info("Setting training size to %d, valid size to %d and test size to \
+                %d words", training_size, valid_size, test_size)
 
     sentences_for_training = reservoir_tokens_number(training_files, training_size)
     sentences_for_valid = reservoir_tokens_number(valid_files, valid_size)
@@ -74,16 +81,17 @@ def sample(output_dir, input_dir, size, seed):
     print_to_file(sentences_for_test, test_files, output_dir, "test")
 
 
-def print_to_file(sentences_idxs, files, output_path, str):
+def print_to_file(sentences_idxs, files, output_path, basename):
     index = 0
     sentence_number = -1
 
-    logger.info("LEN SENTENCES RESERVOIR: {}".format(len(sentences_idxs)))
+    logger.info("LEN SENTENCES RESERVOIR: %d", len(sentences_idxs))
 
-    with open(output_path+str+".txt", "w") as fout_linear, open(output_path+str+".conll", "w") as fout_conll:
+    with open(output_path / basename / ".txt", "w") as fout_linear, \
+        open(output_path / basename / ".conll", "w") as fout_conll:
+
         for filename in files:
-
-            logger.info("reading file {}...".format(filename))
+            logger.info("reading file %s...", filename)
 
             for sentence in cutils.plain_conll_reader(filename):
                 sentence_number += 1
@@ -95,16 +103,18 @@ def print_to_file(sentences_idxs, files, output_path, str):
 
                     index += 1
                     if not sentence_number % 100:
-                        logger.info("sentence n. {}...".format(sentence_number))
+                        logger.info("sentence n. %d...", sentence_number)
 
                     if index == len(sentences_idxs):
                         break
 
 
 def parse(output_dir, input_dir, model_path):
+
     model = udpipe.Model.load(model_path)
 
-    pipeline = udpipe.Pipeline(model, "horizontal", udpipe.Pipeline.DEFAULT, udpipe.Pipeline.DEFAULT, "conllu")
+    pipeline = udpipe.Pipeline(model, "horizontal",
+                               udpipe.Pipeline.DEFAULT, udpipe.Pipeline.DEFAULT, "conllu")
     error = udpipe.ProcessingError()
 
     # Read whole input
@@ -113,7 +123,7 @@ def parse(output_dir, input_dir, model_path):
 
         logger.info("processing file %s/%s", input_dir, filename)
 
-        with open(input_dir+filename) as fin, \
+        with open(input_dir / filename) as fin, \
             open(output_dir.joinpath(f"{basename}.conllu"), "w") as fout:
             text = ''.join(fin.readlines())
 
